b739923db17d521bbcaa76f3ca74fa76f233a89d
Levindo Neto <levindogtn@gmail.com>
2017-06-07 16:53:29 +0200

    ADD: Function prototype of the suffix checker, begging of the comparisons

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
+from utils_check import utils as check
 
-list = []
-
+symbols = [] # List of binary symbols of a code to be checked
+i = 0
+j = 1
 # Add values to the list of verification (manually)
 print("________________________________________________________________")
 print("| Insert the values (in a binary format)                       |")
@@ -10,10 +12,17 @@ print("| Type 'stop' to stop the adding of values                     |")
 print("|______________________________________________________________|\n")
 
 value = input("> ")
-list.append(value)
+symbols.append(value)
 while value.strip() != 'stop':
     value = input("> ")
     if (value != "stop"):
-        list.append(value)
+        symbols.append(value)
 
-print(list)
+''' Starting of the checking '''
+# All of the symbols in the code is checked with each other,
+# so an ordering of the symbols isn't needed
+for i in range(len(symbols)):
+    for j in range(len(symbols)):
+        if(symbols[i] == symbols[j]):
+            pass
+        j+=1
---
+++ utils_check/utils.py
@@ -0,0 +1,7 @@
+''' This function verifies if a symbol_1 is suffix of a symbol_2
+    @:parameter : Strings in a binary format: symbol_1, symbol_2
+    @:return    : BV Graph '''
+def isSuffixOf(symbol_1, symbol_2):
+    #TODO
+    return True
+
